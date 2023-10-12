31a11c34144fefdc40beb4cb4e7f6afb9f9c47df
Benjamin Appiah <ben.appiah451@gmail.com>
2023-10-12 15:41:12 +0000

    add all

--- task_manager/src/App.jsx
+++ task_manager/src/App.jsx
@@ -1,34 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <>
+           <div
+    className=" d-flex align-items-center justify-content-center flex-column"
+   
+  >
+    <h3>Todo App</h3>
+    <div className="d-flex align-items-center mb-3">
+      <div className="form-group mr-3 mb-0">
+        <input
+          type="text"
+          className="form-control"
+          id="formGroupExampleInput"
+          placeholder="Enter a task here"
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <button
+        type="button"
+        className="btn btn-primary mr-3"
+      >
+        Save
+      </button>
+      <button type="button" className="btn btn-warning" >
+        Get Tasks
+      </button>
+    </div>
+   
+  </div>
+      </>
   )
 }
 
